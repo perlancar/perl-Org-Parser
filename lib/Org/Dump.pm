769cd0facd66aa076c328af555705a2100c4c7fa
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2011-09-23 06:30:11 +0700

    Make Org::Dump OO, so it's subclassable

--- lib/Org/Dump.pm
+++ lib/Org/Dump.pm
@@ -9,8 +9,26 @@ use String::Escape qw(elide printable);
 
 # VERSION
 
+sub _dump_ts {
+    my ($self, $ts) = @_;
+    my $dump = "";
+    $dump .= "A " if $ts->is_active;
+    $dump .= $ts->datetime;
+    $dump;
+}
+
 sub dump_element {
     my ($el, $indent_level) = @_;
+    __PACKAGE__->new->_dump($el, $indent_level);
+}
+
+sub new {
+    my ($class) = @_;
+    bless {}, $class;
+}
+
+sub _dump {
+    my ($self, $el, $indent_level) = @_;
     $indent_level //= 0;
     my @res;
 
@@ -39,8 +57,7 @@ sub dump_element {
         #$line .= " mu_end" if $el->{_mu_end}; #TMP
         $line .= " ".$el->style if $el->style;
     } elsif ($type eq 'Timestamp') {
-        $line .= " A" if $el->is_active;
-        $line .= " dt=".$el->datetime;
+        $line .= " ".$self->_dump_ts($el);
     } elsif ($type eq 'TimeRange') {
     } elsif ($type eq 'Drawer') {
         $line .= " ".$el->name;
@@ -55,24 +72,24 @@ sub dump_element {
 
     if ($type eq 'Headline') {
         push @res, "  " x ($indent_level+1), "(title)\n";
-        push @res, dump_element($el->title, $indent_level+1);
+        push @res, $self->_dump($el->title, $indent_level+1);
         push @res, "  " x ($indent_level+1), "(children)\n" if $el->children;
     } elsif ($type eq 'Footnote') {
         if ($el->def) {
             push @res, "  " x ($indent_level+1), "(definition)\n";
-            push @res, dump_element($el->def, $indent_level+1);
+            push @res, $self->_dump($el->def, $indent_level+1);
         }
         push @res, "  " x ($indent_level+1), "(children)\n" if $el->children;
     } elsif ($type eq 'ListItem') {
         if ($el->desc_term) {
             push @res, "  " x ($indent_level+1), "(description term)\n";
-            push @res, dump_element($el->desc_term, $indent_level+1);
+            push @res, $self->_dump($el->desc_term, $indent_level+1);
         }
         push @res, "  " x ($indent_level+1), "(children)\n" if $el->children;
     }
 
     if ($el->children) {
-        push @res, dump_element($_, $indent_level+1) for @{ $el->children };
+        push @res, $self->_dump($_, $indent_level+1) for @{ $el->children };
     }
 
     join "", @res;
@@ -100,6 +117,8 @@ sub _format_properties {
 =head1 FUNCTIONS
 
 None are exported.
+
+=for Pod::Coverage new
 
 =head2 dump_element($elem) => STR
 
